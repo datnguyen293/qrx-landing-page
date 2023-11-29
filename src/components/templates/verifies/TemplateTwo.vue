e4e96077980b65a606d56af5554a67da30bd8275
Your Name <you@example.com>
2023-11-29 18:11:24 +0700

    update: default image

--- src/components/templates/verifies/TemplateTwo.vue
+++ src/components/templates/verifies/TemplateTwo.vue
@@ -114,11 +114,18 @@ const handleEventSubmit = async (event: any) => {
                 STAMP_STATUS.PROCESSING,
                 STATUS_VERIFY.OVER_LIMITED,
                 STAMP_STATUS.WARRANTY_PROCESSING,
-              ].includes(stampCodeStatus) || message.logo === ''
+              ].includes(stampCodeStatus)
             "
           />
           <img
             :src="message.logo"
+            alt="Logo stamp success"
+            class="!w-[200px] !h-[180px]"
+            :class="stampCodeStatus === STATUS_VERIFY.SUCCESS ? 'hidden' : ''"
+            v-else-if="message.logo !== ''"
+          />
+          <img
+            src="@/assets/images/icon-hero.png"
             alt="Logo stamp success"
             class="!w-[200px] !h-[180px]"
             :class="stampCodeStatus === STATUS_VERIFY.SUCCESS ? 'hidden' : ''"
